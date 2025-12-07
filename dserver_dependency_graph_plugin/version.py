652501f643e73bf5a1f87788d4c4b707dde34ce7
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2025-12-07 23:25:45 +0100

    BUILD: Switched build system to flit

--- dserver_dependency_graph_plugin/version.py
+++ dserver_dependency_graph_plugin/version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.4.3.dev1'
-__version_tuple__ = version_tuple = (0, 4, 3, 'dev1')
+__version__ = version = '0.4.3.dev2'
+__version_tuple__ = version_tuple = (0, 4, 3, 'dev2')
 
-__commit_id__ = commit_id = 'gcde515931'
+__commit_id__ = commit_id = 'g8c317d2de'
